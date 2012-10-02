e3262fa4f1c599dbe0d418d93cd5921911bfe7ea
Stephen Early <steve@greenend.org.uk>
2012-10-02 16:59:17 +0100

    Skeleton of Django-based web service

---
+++ server/fvlogging/fvlogging/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'fvlogging.views.home', name='home'),
+    # url(r'^fvlogging/', include('fvlogging.foo.urls')),
+
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+)
---
+++ server/fvserial.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+# This script presents the fvcontroller RS485 interface as a
+# socket-based service.  Only one connection is dealt with at once;
+# others are queued.
+
+# The script ensures that no controller is selected between
+# connections, and that controllers are in a known state (empty rxbuf,
+# nobody transmitting) waiting for commands.  It provides an explicit
+# TIMEOUT response if a controller does not respond, and deals with
+# returning the RS485 network to the default state if a timeout
+# occurs.
+
+import serial
+import SocketServer
+
+def full_reset(s):
+    """Return the bus to a known state: terminate any previous command
+    with \n, send a SELECT NONE\n, and discard everything from the
+    receive buffer.
+
+    """
+    # Terminate any partly-sent command.  We have to wait after
+    # sending this for up to 0.1s for any output from the currently
+    # selected controller to be completed; we receive and discard this
+    # output.  No controller will send more than one line.
+    s.write("\n")
+    old_timeout=s.timeout
+    s.timeout=0.1
+    s.readline()
+    s.timeout=old_timeout
+    s.write("SELECT NONE\n")
+    
+class ConnectionHandler(SocketServer.StreamRequestHandler):
+    def handle(self):
+        for data in self.rfile:
+            data=data.strip()
+            s.write("%s\n"%data)
+            response=s.readline()
+            if response=="": response="TIMEOUT\n"
+            if response[-1]!="\n": response="CORRUPT\n"
+            try:
+                self.wfile.write(response)
+            except:
+                break
+        s.write("SELECT NONE\n")
+    
+if __name__=="__main__":
+    HOST,PORT="localhost",1576
+
+    server=SocketServer.TCPServer((HOST,PORT),ConnectionHandler)
+
+    s=serial.Serial("/dev/ttyUSB0",timeout=1)
+    full_reset(s)
+
+    server.serve_forever()
