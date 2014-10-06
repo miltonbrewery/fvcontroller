7968659332f0f18755183479ad77945006b6f330
Stephen Early <steve@greenend.org.uk>
2014-10-06 14:05:17 +0100

    Add new registers to controller update command

--- server/fvlogging/datalog/management/commands/makecontroller.py
+++ server/fvlogging/datalog/management/commands/makecontroller.py
@@ -3,30 +3,44 @@
 from django.core.management.base import BaseCommand, CommandError
 from datalog.models import Controller,Register
 
+modes=4
+
 # Name, description, datatype, unit, readonly, max_interval, config
 regs=[
     ('t0','Temperature','F',u'°C',True,30,False),
     ('v0','Valve state','S','',True,30,False),
     ('mode','Mode','S','',False,30,False),
+    ('alarm','Alarm','S','',True,30,False),
     ('set/lo','Low set point','F',u'°C',False,30,False),
     ('set/hi','High set point','F',u'°C',False,30,False),
+    ('alarm/lo','Alarm low set point','F',u'°C',False,30,False),
+    ('alarm/hi','Alarm high set point','F',u'°C',False,30,False),
+    ('jog/lo','Stuck valve low set point','F',u'°C',False,30,False),
+    ('jog/hi','Stuck valve high set point','F',u'°C',False,30,False),
     ('ver','Firmware version','S','',True,300,True),
     ('flashcnt','Firmware reflash count','I','',True,300,True),
     ('bl','Backlight timeout','I','cs',False,300,True),
+    ('bl/alarm','Alarm flash period','I','cs',False,300,True),
+    ('jog/flip','How long to blip valve to unstick it','I','cs',False,300,True),
+    ('jog/wait','Time between attempts to unstick valve','I','cs',False,300,True),
     ('t0/id','Probe ID','S','',False,300,True),
-    ('m0/name','Mode 0 name','S','',False,300,True),
-    ('m0/lo','Mode 0 low set point','S','',False,300,True),
-    ('m0/hi','Mode 0 high set point','S','',False,300,True),
-    ('m1/name','Mode 1 name','S','',False,300,True),
-    ('m1/lo','Mode 1 low set point','S','',False,300,True),
-    ('m1/hi','Mode 1 high set point','S','',False,300,True),
-    ('m2/name','Mode 2 name','S','',False,300,True),
-    ('m2/lo','Mode 2 low set point','S','',False,300,True),
-    ('m2/hi','Mode 2 high set point','S','',False,300,True),
-    ('m3/name','Mode 3 name','S','',False,300,True),
-    ('m3/lo','Mode 3 low set point','S','',False,300,True),
-    ('m3/hi','Mode 3 high set point','S','',False,300,True),
 ]
+
+moderegs=[
+    ('m{mnum}/name','Mode {mnum} name'),
+    ('m{mnum}/lo','Mode {mnum} low set point'),
+    ('m{mnum}/hi','Mode {mnum} high set point'),
+    ('m{mnum}/a/lo','Mode {mnum} alarm low set point'),
+    ('m{mnum}/a/hi','Mode {mnum} alarm high set point'),
+    ('m{mnum}/j/lo','Mode {mnum} stuck valve low set point'),
+    ('m{mnum}/j/hi','Mode {mnum} stuck valve high set point'),
+]
+
+allregs=regs
+for mnum in range(0,modes):
+    for name,desc in moderegs:
+        allregs.append((name.format(mnum=mnum),desc.format(mnum=mnum),
+                        'S','',False,300,True))
 
 class Command(BaseCommand):
     args='controllername'
@@ -42,12 +56,15 @@ class Command(BaseCommand):
             controller=Controller(ident=cname,description=cname,
                                   address="localhost",port=1576,active=True)
             controller.save()
-        for name,desc,datatype,unit,readonly,timeout,config in regs:
+
+        for name,desc,datatype,unit,readonly,timeout,config in allregs:
             try:
                 reg=Register.objects.get(controller=controller,name=name)
+                print "%s exists"%name
             except Register.DoesNotExist:
-                print "Creating %s..."%name
+                print "%s does not exist; creating..."%name
                 reg=Register(controller=controller,name=name,description=desc,
                              datatype=datatype,unit=unit,readonly=readonly,
-                             max_interval=timeout,config=config)
+                             max_interval=timeout,config=config,
+                             frontpage=False)
                 reg.save()
