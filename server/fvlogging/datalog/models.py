8bd60684c135cf8b6a7a3a18e4ea89ebe66f16dc
Stephen Early <steve@assorted.org.uk>
2019-04-18 13:13:11 +0100

    Reformat
    Get a bit closer to PEP-8 style formatting

--- server/fvlogging/datalog/models.py
+++ server/fvlogging/datalog/models.py
@@ -1,78 +1,84 @@
 from django.db import models
 from django.urls import reverse
 import socket
-import time,datetime
-try:
-    import django.utils.timezone
-    now=django.utils.timezone.now
-except:
-    now=datetime.datetime.now
+import time
+import datetime
+import django.utils.timezone
+now = django.utils.timezone.now
 
 class Controller(models.Model):
-    """A controller that can be present on a RS485 bus.  Has a number
-    of registers (defined in a separate model).  Accessed by
-    connecting to a TCP port, sending a SELECT ident command, and
+    """A controller that can be present on a RS485 bus.
+
+    Has a number of registers (defined in a separate model).  Accessed
+    by connecting to a TCP port, sending a SELECT ident command, and
     using READ and SET commands.
-
     """
-    ident=models.CharField(max_length=8)
-    description=models.TextField()
-    address=models.TextField()
-    port=models.IntegerField()
-    active=models.BooleanField()
-    def connect(self):
-        """Return a file-like connection to the RS485 bus, with this
-        controller selected, or None if there is a failure.
+    ident = models.CharField(max_length=8)
+    description = models.TextField()
+    address = models.TextField()
+    port = models.IntegerField()
+    active = models.BooleanField()
 
+    def connect(self):
+        """Connect to this controller.
+
+        Returns a file-like connection to the RS485 bus, with this
+        controller selected, or None if there is a failure.
         """
-        if not self.active: return None
-        s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        if not self.active:
+            return None
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
-            s.connect((self.address,self.port))
+            s.connect((self.address, self.port))
         except:
             return None
         s.settimeout(1.5)
-        s=s.makefile('rw')
-        s.write("SELECT %s\n"%self.ident)
+        s = s.makefile('rw')
+        s.write("SELECT %s\n" % self.ident)
         s.flush()
-        response=s.readline()
-        if response!="OK %s selected\n"%self.ident:
+        response = s.readline()
+        if response != "OK %s selected\n" % self.ident:
             s.close()
             return None
         return s
-    def read(self,register):
-        """Read a register as a string.
 
+    def read(self, register):
+        """Read a register as a string.
         """
-        s=self.connect()
-        if not s: return None # Maybe raise exception instead?
+        s = self.connect()
+        if not s:
+            return None # Maybe raise exception instead?
         try:
-            s.write("READ %s\n"%register)
+            s.write("READ %s\n" % register)
             s.flush()
-            response=s.readline().strip()
-            if response[0:3]!="OK ": return None
+            response = s.readline().strip()
+            if response[0:3] != "OK ":
+                return None
             return response[3:]
         finally:
             s.close()
-    def write(self,register,value):
-        """Write a string to a register.
 
+    def write(self, register, value):
+        """Write a string to a register.
         """
-        s=self.connect()
-        if not s: return None # Exception?
+        s = self.connect()
+        if not s:
+            return None # Exception?
         try:
-            s.write("SET %s %s\n"%(register,value))
+            s.write("SET %s %s\n" % (register, value))
             s.flush()
-            response=s.readline().strip()
+            response = s.readline().strip()
             return response
         finally:
             s.close()
+
     def regs(self):
         """Return register set as a dict for use in templates.
-        Templates don't allow the '/' character, so remove it.
 
+        Templates don't allow the '/' character, so remove it.
         """
-        return {x.name.replace('/',''):x for x in self.register_set.all()}
+        return {x.name.replace('/', ''): x for x in self.register_set.all()}
+
     def __str__(self):
         return self.ident
 
@@ -81,115 +87,126 @@ class Controller(models.Model):
 
 class Datum(models.Model):
     class Meta:
-        abstract=True
-    register=models.ForeignKey("Register", on_delete=models.PROTECT)
-    timestamp=models.DateTimeField()
+        abstract = True
+    register = models.ForeignKey("Register", on_delete=models.PROTECT)
+    timestamp = models.DateTimeField()
+
     @staticmethod
     def cast(value):
         return value
 
 class StringDatum(Datum):
-    data=models.TextField(null=True)
+    data = models.TextField(null=True)
     def __str__(self):
         return self.data
 
 class FloatDatum(Datum):
-    data=models.FloatField(null=True)
+    data = models.FloatField(null=True)
     def __str__(self):
-        if self.data: return "%0.2f"%self.data
+        if self.data:
+            return "%0.2f"%self.data
         return "None"
+
     @staticmethod
     def cast(value):
-        if value=="None": return None
+        if value == "None":
+            return None
         return float(value)
 
 class IntegerDatum(Datum):
-    data=models.IntegerField(null=True)
+    data = models.IntegerField(null=True)
     def __str__(self):
-        if self.data: return "%d"%self.data
+        if self.data:
+            return "%d" % self.data
         return "None"
+
     @staticmethod
     def cast(value):
-        if value=="None": return None
+        if value == "None":
+            return None
         return int(value)
 
-DATATYPES=(
-    ('S',"StringDatum"),
-    ('F',"FloatDatum"),
-    ('I',"IntegerDatum"),
+DATATYPES = (
+    ('S', "StringDatum"),
+    ('F', "FloatDatum"),
+    ('I', "IntegerDatum"),
 )
 
-DATATYPE_DICT={
-    'S':StringDatum,
-    'F':FloatDatum,
-    'I':IntegerDatum,
+DATATYPE_DICT = {
+    'S': StringDatum,
+    'F': FloatDatum,
+    'I': IntegerDatum,
 }
 
 class Register(models.Model):
-    """A register found in a controller.  We log values found in these
-    registers at regular intervals.
+    """A register found in a controller.
 
+    We log values found in these registers at regular intervals.
     """
-    controller=models.ForeignKey(Controller, on_delete=models.PROTECT)
-    name=models.CharField(max_length=8)
-    description=models.TextField()
-    # Something to indicate the datatype - there will be classes for each one
+    controller = models.ForeignKey(Controller, on_delete=models.PROTECT)
+    name = models.CharField(max_length=8)
+    description = models.TextField()
+    # Something to indicate the datatype - there are classes for each one
     # descending from an abstract base class
-    datatype=models.CharField(max_length=1,choices=DATATYPES)
-    unit=models.CharField(max_length=10,null=True,blank=True)
-    readonly=models.BooleanField(
+    datatype = models.CharField(max_length=1, choices=DATATYPES)
+    unit = models.CharField(max_length=10, null=True, blank=True)
+    readonly = models.BooleanField(
         help_text="Can value not be set on controller?")
     # If the most recent recorded value is older than this, read it
     # again from the hardware rather than the database
-    max_interval=models.IntegerField() # In seconds
-    config=models.BooleanField() # Is this a configuration register?
-    frontpage=models.BooleanField(
+    max_interval = models.IntegerField() # In seconds
+    config = models.BooleanField() # Is this a configuration register?
+    frontpage = models.BooleanField(
         help_text="Show this register on the site front page?")
-    graphcolour=models.CharField(
-        max_length=20,blank=True,
+    graphcolour = models.CharField(
+        max_length=20, blank=True,
         help_text="Colour of trace on controller's default graph, or "
         "blank to leave out")
-    graphcolour_all=models.CharField(
-        max_length=20,blank=True,
+    graphcolour_all = models.CharField(
+        max_length=20, blank=True,
         help_text="Colour of trace on all graphs, or blank to leave out")
+
     def __str__(self):
-        return "%s %s"%(self.controller,self.name)
-    def value(self,force_check=False):
+        return "%s %s" % (self.controller, self.name)
+
+    def value(self, force_check=False):
         # Read most recent (up to) two datapoints.
-        dt=DATATYPE_DICT[self.datatype]
-        dpl=dt.objects.filter(register=self).order_by('-timestamp')[:2]
+        dt = DATATYPE_DICT[self.datatype]
+        dpl = dt.objects.filter(register=self).order_by('-timestamp')[:2]
         # If there are zero datapoints, we always record a new one.
         # Otherwise, we check to see how old the most recent datapoint
         # is, and consider recording a new one if it is more than
         # max_interval seconds old.
-        if len(dpl)==0 or force_check or (
-            (now()-dpl[0].timestamp)
+        if len(dpl) == 0 or force_check or (
+            (now() - dpl[0].timestamp)
             > datetime.timedelta(seconds=self.max_interval)):
-            r=self.controller.read(self.name)
+            r = self.controller.read(self.name)
             if not r:
                 # Reading from the hardware failed.  We return the most
                 # recent value if there is one, or None.
-                if len(dpl)>0: return dpl[0]
-                else: return None
-            val=dt.cast(r)
-            if (len(dpl)>0 and val==dpl[0].data and len(dpl)==2 and
-                dpl[0].data==dpl[1].data):
+                if len(dpl)>0:
+                    return dpl[0]
+                else:
+                    return None
+            val = dt.cast(r)
+            if (len(dpl) > 0 and val == dpl[0].data and len(dpl) == 2 and
+                dpl[0].data == dpl[1].data):
                 # No change, and we already have two datapoints in a
                 # row with this value.  We just update the timestamp
                 # on the most recent.
-                dpl[0].timestamp=now()
+                dpl[0].timestamp = now()
                 dpl[0].save()
-                dp=dpl[0]
+                dp = dpl[0]
             else:
                 # We record a new datapoint.
-                dp=dt(register=self,timestamp=now(),
-                      data=val)
+                dp = dt(register=self, timestamp=now(), data=val)
                 dp.save()
         else:
             # Return the most recent datapoint - it isn't time to check
             # the hardware yet
-            dp=dpl[0]
+            dp = dpl[0]
         return dp
+
     def last_change(self):
         """When did the value last change?
 
@@ -197,8 +214,8 @@ class Register(models.Model):
         database.
         """
         # Read most recent (up to) two datapoints.
-        dt=DATATYPE_DICT[self.datatype]
-        dpl=dt.objects.filter(register=self).order_by('-timestamp')[:2]
+        dt = DATATYPE_DICT[self.datatype]
+        dpl = dt.objects.filter(register=self).order_by('-timestamp')[:2]
         if len(dpl) == 0:
             return
         elif len(dpl) == 1:
@@ -207,20 +224,26 @@ class Register(models.Model):
             if dpl[0].data == dpl[1].data:
                 return dpl[1].timestamp
             return dpl[0].timestamp
-    def set(self,value):
-        rv=self.controller.write(self.name,value)
+
+    def set(self, value):
+        """Set a new value for this register
+
+        Errors are currently ignored and the register value does not change.
+        This could occur if the hardware is offline, for example.
+        """
+        rv = self.controller.write(self.name, value)
         # After setting a register we force a readback from the hardware,
         # ignoring the timestamp on the most recently recorded datapoint.
         self.value(force_check=True)
         return rv
 
 class NoteType(models.Model):
-    desc=models.TextField()
+    desc = models.TextField()
 
 class Note(models.Model):
-    controller=models.ForeignKey(Controller, on_delete=models.CASCADE)
-    type=models.ForeignKey(NoteType, on_delete=models.CASCADE)
-    timestamp=models.DateTimeField()
-    data=models.TextField()
+    controller = models.ForeignKey(Controller, on_delete=models.CASCADE)
+    type = models.ForeignKey(NoteType, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField()
+    data = models.TextField()
     class Meta:
-        ordering=['-timestamp']
+        ordering = ['-timestamp']
--- server/fvlogging/datalog/views.py
+++ server/fvlogging/datalog/views.py
@@ -1,4 +1,6 @@
-from django.http import HttpResponse,HttpResponseRedirect,Http404
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.http import Http404
 from django.template import RequestContext
 from django.shortcuts import render
 from django import forms
@@ -9,20 +11,23 @@ import csv
 import xml.etree.ElementTree as ET
 
 def summary(request):
-    controllers=Controller.objects.all()
-    registers=Register.objects.filter(frontpage=True).order_by("description").all()
+    controllers = Controller.objects.all()
+    registers = Register.objects.filter(frontpage=True)\
+                                .order_by("description")\
+                                .all()
     return render(request, 'datalog/summary.html',
                   context={'controllers': controllers,
                            'registers': registers})
 
-def detail(request,name,config=False):
+def detail(request, name, config=False):
     try:
-        controller=Controller.objects.get(ident=name)
-    except:
+        controller = Controller.objects.get(ident=name)
+    except Controller.DoesNotExist:
         raise Http404
-    registers=controller.register_set.filter(config=config)
-    extraseries=Register.objects.exclude(graphcolour_all="")
-    if request.method=='POST':
+    registers = controller.register_set.filter(config=config)
+    extraseries = Register.objects.exclude(graphcolour_all="")
+
+    if request.method == 'POST':
         # Go through the registers looking for ones that have a value
         # set in the request, and update as appropriate.
         for r in registers:
@@ -32,54 +37,58 @@ def detail(request,name,config=False):
             reverse('datalog-controller-config' if config
                     else 'datalog-controller',
                     args=(name,)))
+
     return render(request, 'datalog/detail.html',
                   context={'controller': controller,
                            'registers': registers,
                            'extraseries': extraseries,
-                           'config': config,})
+                           'config': config,
+                  })
 
 class GraphPeriodForm(forms.Form):
-    start=forms.DateTimeField()
-    end=forms.DateTimeField()
+    start = forms.DateTimeField()
+    end = forms.DateTimeField()
     def clean(self):
-        s=None
-        e=None
+        s = None
+        e = None
         try:
-            s=self.cleaned_data['start']
-            e=self.cleaned_data['end']
+            s = self.cleaned_data['start']
+            e = self.cleaned_data['end']
         except:
             pass
-        if s and e and e<=s:
+        if s and e and e <= s:
             raise forms.ValidationError("Start must be before end")
         return self.cleaned_data
 
-def detailgraph(request,name,start=None,end=None):
+def detailgraph(request, name, start=None, end=None):
     try:
-        controller=Controller.objects.get(ident=name)
-    except:
+        controller = Controller.objects.get(ident=name)
+    except Controller.DoesNotExist:
         raise Http404
-    extraseries=Register.objects.exclude(graphcolour_all="")
-    if start==None or end==None:
-        end=datetime.datetime.now().replace(microsecond=0)
-        start=end-datetime.timedelta(days=7)
+    extraseries = Register.objects.exclude(graphcolour_all="")
+    if start == None or end == None:
+        end = datetime.datetime.now().replace(microsecond=0)
+        start = end - datetime.timedelta(days=7)
     else:
-        start=parsedatetime(start)
-        end=parsedatetime(end)
-    if request.method=='POST':
-        form=GraphPeriodForm(request.POST)
+        start = parsedatetime(start)
+        end = parsedatetime(end)
+
+    if request.method == 'POST':
+        form = GraphPeriodForm(request.POST)
         if form.is_valid():
-            cd=form.cleaned_data
-            start=cd['start']
-            end=cd['end']
+            cd = form.cleaned_data
+            start = cd['start']
+            end = cd['end']
             return HttpResponseRedirect(
                 reverse('datalog-detailgraph-period',
                         args=(name, str(start), str(end))))
     else:
-        form=GraphPeriodForm(initial={
-            'start':start,
-            'end':end})
+        form = GraphPeriodForm(initial={
+            'start': start,
+            'end': end,
+        })
 
-    period=end-start
+    period = end - start
 
     return render(request, 'datalog/detailgraph.html',
                   context={'controller': controller,
@@ -88,38 +97,39 @@ def detailgraph(request,name,start=None,end=None):
                            'start': str(start),
                            'end': str(end),
                            'period': period,
-                           'back': str(start-period),
-                           'forward': str(end+period),
+                           'back': str(start - period),
+                           'forward': str(end + period),
                   })
 
-def series_csv(request,name,register):
+def series_csv(request, name, register):
     try:
         controller=Controller.objects.get(ident=name)
-    except:
+    except Controller.DoesNotExist:
         raise Http404
     try:
-        register=controller.register_set.get(name=register)
-    except:
+        register = controller.register_set.get(name=register)
+    except Register.DoesNotExist:
         raise Http404
-    dt=DATATYPE_DICT[register.datatype]
-    series=dt.objects.filter(register=register).order_by('timestamp')
-    r=HttpResponse(content_type="text/csv")
-    r['Content-Disposition']='attachment; filename=%s-%s.csv'%(
-        controller.ident,register.name.replace('/','-'))
-    c=csv.writer(r)
+    dt = DATATYPE_DICT[register.datatype]
+    series = dt.objects.filter(register=register).order_by('timestamp')
+    r = HttpResponse(content_type="text/csv")
+    r['Content-Disposition'] = 'attachment; filename=%s-%s.csv' % (
+        controller.ident, register.name.replace('/', '-'))
+    c = csv.writer(r)
     for d in series:
-        c.writerow((d.timestamp.strftime("%Y-%m-%d %H:%M:%S"),d.data))
+        c.writerow((d.timestamp.strftime("%Y-%m-%d %H:%M:%S"), d.data))
     return r
 
 def parsedatetime(d):
-    """Convert a string to a datetime object, trying several possible
-    format strings in order.  Returns the first match, or None.
+    """Convert a string to a datetime object
 
+    Tries several possible format strings in order.  Returns the first
+    match, or None.
     """
-    formats=('%Y-%m-%d %H:%M:%S','%Y-%m-%d %H:%M','%Y-%m-%d')
+    formats = ('%Y-%m-%d %H:%M:%S', '%Y-%m-%d %H:%M', '%Y-%m-%d')
     for f in formats:
         try:
-            return datetime.datetime.strptime(d,f)
+            return datetime.datetime.strptime(d, f)
         except ValueError:
             pass
     return None
@@ -137,101 +147,102 @@ def graph(request):
     title - title of graph
     series - a data series to plot, as follows:
       controller:register:colour
-      
     """
-    width=int(request.GET.get('width',800))
-    height=int(request.GET.get('height',600))
-    leftmargin=int(request.GET.get('leftmargin',100))
-    bottommargin=int(request.GET.get('bottommargin',100))
-    floatmin=float(request.GET.get('floatmin',0.0))
-    floatmax=float(request.GET.get('floatmax',30.0))
-    end=None
+    width = int(request.GET.get('width', 800))
+    height = int(request.GET.get('height', 600))
+    leftmargin = int(request.GET.get('leftmargin', 100))
+    bottommargin = int(request.GET.get('bottommargin', 100))
+    floatmin = float(request.GET.get('floatmin', 0.0))
+    floatmax = float(request.GET.get('floatmax', 30.0))
+    end = None
     if 'end' in request.GET:
-        end=parsedatetime(request.GET['end'])
-    if end is None: end=datetime.datetime.now()
-    start=None
+        end = parsedatetime(request.GET['end'])
+    if end is None:
+        end = datetime.datetime.now()
+    start = None
     if 'start' in request.GET:
-        start=parsedatetime(request.GET['start'])
+        start = parsedatetime(request.GET['start'])
         if start is None:
             try:
-                start=end-datetime.timedelta(days=int(request.GET['start']))
+                start = end - datetime.timedelta(days=int(request.GET['start']))
             except ValueError:
                 pass
     if start is None:
-        start=end-datetime.timedelta(days=30)
+        start = end - datetime.timedelta(days=30)
 
-    length=(end-start).total_seconds()
-    series=request.GET.getlist('series')
+    length = (end - start).total_seconds()
+    series = request.GET.getlist('series')
 
     # Origin is in top-left by default.
-    svg=ET.Element("svg",{'xmlns':'http://www.w3.org/2000/svg',
-                          'version':'1.1',
-                          'width':str(width),'height':str(height)})
-    ET.SubElement(svg,"title").text="A Graph"
-    ET.SubElement(svg,"desc").text="A longer description of the group"
+    svg = ET.Element("svg", {'xmlns': 'http://www.w3.org/2000/svg',
+                             'version': '1.1',
+                             'width': str(width),
+                             'height': str(height)})
+    ET.SubElement(svg, "title").text = "A Graph"
+    ET.SubElement(svg, "desc").text = "A longer description of the group"
     # Bounding box
-    ET.SubElement(svg,"rect",x="0",y="0",width=str(width),height=str(height),
-                  fill="none",stroke="blue")
+    ET.SubElement(svg, "rect", x="0", y="0",
+                  width=str(width), height=str(height),
+                  fill="none", stroke="blue")
 
     # Let's create a group with a transformed coordinate space such
-    # that the origin is in the bottom-left, 100 pixels in and 100
-    # pixels from the bottom.
-    g=ET.SubElement(svg,"g",transform="translate(%d,%d) scale(1,-1)"%(
-            leftmargin,height-bottommargin))
+    # that the origin is in the bottom-left
+    g = ET.SubElement(svg, "g", transform="translate(%d,%d) scale(1,-1)" % (
+        leftmargin, height - bottommargin))
     
-    # We now can draw axes, etc. from 0-(width-leftmargin) on X and 0-(height-bottommargin) on y
-    graphwidth=width-leftmargin
-    graphheight=height-bottommargin
+    # We now can draw axes, etc. from 0 - (width - leftmargin) on X and 0 - (height - bottommargin) on y
+    graphwidth = width - leftmargin
+    graphheight = height - bottommargin
 
     # Draw horizontal scale lines for temperature
-    y=0.0
-    while y<floatmax:
-        scaley=(y-floatmin)*graphheight/floatmax
-        ET.SubElement(g,"line",x1="0",x2=str(width),
-                      y1=str(scaley),y2=str(scaley),
+    y = 0.0
+    while y < floatmax:
+        scaley = (y - floatmin) * graphheight / floatmax
+        ET.SubElement(g, "line", x1="0", x2=str(width),
+                      y1=str(scaley), y2=str(scaley),
                       stroke="lightgrey")
-        y=y+1.0
-    y=0.0
-    while y<floatmax:
-        scaley=(y-floatmin)*graphheight/floatmax
-        ET.SubElement(g,"line",x1="0",x2=str(width),
-                      y1=str(scaley),y2=str(scaley),
+        y = y + 1.0
+    y = 0.0
+    while y < floatmax:
+        scaley = (y - floatmin) * graphheight / floatmax
+        ET.SubElement(g, "line", x1="0", x2=str(width),
+                      y1=str(scaley), y2=str(scaley),
                       stroke="darkgrey")
-        y=y+5.0
+        y = y + 5.0
 
     for s in series:
-        controller,register,colour=s.split(":")
-        controller=Controller.objects.get(ident=controller)
-        register=Register.objects.get(controller=controller,name=register)
+        controller, register, colour = s.split(":")
+        controller = Controller.objects.get(ident=controller)
+        register = Register.objects.get(controller=controller, name=register)
         # Retrieve all datapoints between start and end
-        dt=DATATYPE_DICT[register.datatype]
+        dt = DATATYPE_DICT[register.datatype]
         # We want to find the timestamps of the first datapoint before
         # start (if there is one) and the first datapoint after end
         # (if there is one), and adjust start and end to include
         # these.  This will avoid the graph having blank sections at
         # the left and right hand sides.
-        sdp=dt.objects.filter(register=register,timestamp__lt=start).\
-             order_by('-timestamp')[:1]
-        s_start=sdp[0].timestamp if len(sdp)==1 else start
-        edp=dt.objects.filter(register=register,timestamp__gt=end).\
-             order_by('timestamp')[:1]
-        s_end=edp[0].timestamp if len(edp)==1 else end
-        datapoints=dt.objects.filter(register=register,timestamp__lte=s_end,
-                                     timestamp__gte=s_start).\
-            order_by('timestamp')
-        now_drawing=False
-        dl=[]
+        sdp = dt.objects.filter(register=register, timestamp__lt=start)\
+                        .order_by('-timestamp')[:1]
+        s_start = sdp[0].timestamp if len(sdp) == 1 else start
+        edp = dt.objects.filter(register=register,timestamp__gt=end)\
+                        .order_by('timestamp')[:1]
+        s_end = edp[0].timestamp if len(edp) == 1 else end
+        datapoints = dt.objects.filter(register=register, timestamp__lte=s_end,
+                                       timestamp__gte=s_start)\
+                               .order_by('timestamp')
+        now_drawing = False
+        dl = []
         for datum in datapoints:
-            x=(datum.timestamp-start).total_seconds()*graphwidth/length
+            x = (datum.timestamp - start).total_seconds() * graphwidth / length
             if datum.data is None:
-                now_drawing=False
+                now_drawing = False
             else:
                 # Let's assume float for now
-                y=(datum.data-floatmin)*graphheight/floatmax
-                dl.append("%s %f %f"%('L' if now_drawing else 'M',x,y))
-                now_drawing=True
-        ET.SubElement(g,"path",stroke=colour,fill="none",d=" ".join(dl))
+                y = (datum.data - floatmin) * graphheight / floatmax
+                dl.append("%s %f %f" % ('L' if now_drawing else 'M', x, y))
+                now_drawing = True
+        ET.SubElement(g, "path", stroke=colour, fill="none", d=" ".join(dl))
 
-    r=HttpResponse(content_type="image/svg+xml")
-    ET.ElementTree(svg).write(r,encoding="UTF-8",xml_declaration=True)
+    r = HttpResponse(content_type="image/svg+xml")
+    ET.ElementTree(svg).write(r, encoding="UTF-8", xml_declaration=True)
     return r
